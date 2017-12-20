a4c35c2dd8167c03d71df6abca4fc7c9a50d6a77
Andrey <matradze@gmail.com>
2017-12-20 11:57:51 +0700

    up work

---
+++ www/scripts/main.js
@@ -0,0 +1,31 @@
+/*button delete*/
+/*Header*/
+$(function () {
+    $("#header").load("header.html");
+});
+
+
+$(document).ready(function () {
+    /*Table*/
+    $('.tableExample1').DataTable({
+        "dom": 't',
+        "iDisplayLength": 100
+    });
+
+    $('.tableExample2').DataTable({
+        "dom": "<'row'<'col-lg-6'l><'col-lg-6'f>>t<'row'<'col-lg-6'i><'col-lg-6'p>>",
+        "lengthMenu": [[6, 25, 50, -1], [6, 25, 50, "All"]],
+        "iDisplayLength": 6
+    });
+
+    $('.tableExample3').DataTable({
+        dom: "<'row'<'col-sm-4'l><'col-sm-4 text-center'B><'col-sm-4'f>>tp",
+        "lengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]],
+        buttons: [
+            {extend: 'copy', className: 'btn-sm'},
+            {extend: 'csv', title: 'ExampleFile', className: 'btn-sm'},
+            {extend: 'pdf', title: 'ExampleFile', className: 'btn-sm'},
+            {extend: 'print', className: 'btn-sm'}
+        ]
+    });
+});
